253d0da1ae3a335160397810dbc3c6784f904baf
Semi Eren Gokyildiz <127510869+SERENGOKYILDIZ@users.noreply.github.com>
2025-11-14 16:43:25 +0300

    Learned File Operations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:android_flutter_storage_operations/sayfaA.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,23 +29,47 @@ class Anasayfa extends StatefulWidget {
 
 class _AnasayfaState extends State<Anasayfa> {
 
-  Future<void> veriKaydi() async {
+  var tfGirdi = TextEditingController();
 
-    /// await ile oluşmasini bekler
-    var sp = await SharedPreferences.getInstance();
+  Future<void> veriYaz() async {
+    var ad = await getApplicationDocumentsDirectory();
 
-    sp.setString("ad", "Ahmet");
-    sp.setInt("yas", 20);
-    sp.setDouble("boy", 1.75);
-    sp.setBool("durum", false);
+    var uygulamaDosyalamaYolu = await ad.path;
 
-    var arkadaslar = <String>[];
-    arkadaslar.add("Mustafa");
-    arkadaslar.add("Ali");
-    arkadaslar.add("Buse");
-    arkadaslar.add("Ceylin");
+    var dosya = File("${uygulamaDosyalamaYolu}/dosyam.txt");
 
-    sp.setStringList("agalar", arkadaslar);
+    dosya.writeAsString(tfGirdi.text);
+
+    tfGirdi.text = "";
+  }
+
+  Future<void> veriOku() async {
+    try{
+      var ad = await getApplicationDocumentsDirectory();
+
+      var uygulamaDosyalamaYolu = await ad.path;
+
+      var dosya = File("${uygulamaDosyalamaYolu}/dosyam.txt");
+
+      String okunanVeri = await dosya.readAsString();
+
+      tfGirdi.text = okunanVeri;
+    }
+    catch(e){
+      e.toString();
+    }
+  }
+
+  Future<void> veriSil() async {
+    var ad = await getApplicationDocumentsDirectory();
+
+    var uygulamaDosyalamaYolu = await ad.path;
+
+    var dosya = File("${uygulamaDosyalamaYolu}/dosyam.txt");
+
+    if(dosya.existsSync()){
+     dosya.delete();
+    }
   }
 
   @override
@@ -59,15 +83,38 @@ class _AnasayfaState extends State<Anasayfa> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              child: Text("Geçiş Yap"),
-              onPressed: (){
-
-                veriKaydi();
-
-                Navigator.push(context, MaterialPageRoute(builder: (context) => SayfaA()));
-              },
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: tfGirdi,
+                decoration: InputDecoration(
+                  hintText: "Veri Giriniz"
+                ),
+              ),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  child: Text("Yaz"),
+                  onPressed: (){
+                    veriYaz();
+                  },
+                ),
+                ElevatedButton(
+                  child: Text("Oku"),
+                  onPressed: (){
+                    veriOku();
+                  },
+                ),
+                ElevatedButton(
+                  child: Text("Sil"),
+                  onPressed: (){
+                    veriSil();
+                  },
+                ),
+              ],
+            )
           ],
         ),
       ),
